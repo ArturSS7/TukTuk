0209b1707670fcf5a8e187530ef5bf41a28704ea
ArturSS7 <52138851+ArturSS7@users.noreply.github.com>
2020-08-05 10:45:07 +0300

    add ftp to db

--- db_init.sh
+++ db_init.sh
@@ -2,4 +2,5 @@ psql -U postgres -c "create database tuktuk;"
 psql -U postgres -c "create user tuk with encrypted password ZuppaSecurePwd;"
 psql -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE tuktuk TO tuk;"
 psql -U tuk -c "create table http(id serial, data varchar, source_ip varchar, time varchar);"
+psql -U tuk -c "create table ftp(id serial, data varchar, source_ip varchar, time varchar);"
 
