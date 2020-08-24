6c6cbcbff6b383358072e5fdbf6fe2874fc50b4f
Dmit <dmitriy8096@gmail.com>
2020-08-24 18:15:39 +0300

    ldap listener

---
+++ ldaplistener/ldap.go
@@ -0,0 +1,53 @@
+// Listen to 10389 port for LDAP Request
+// and route bind request to the handleBind func
+package ldaplistener
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	ldap "github.com/vjeantet/ldapserver"
+)
+
+func start() {
+	//ldap logger
+	ldap.Logger = log.New(os.Stdout, "[server] ", log.LstdFlags)
+
+	//Create a new LDAP Server
+	server := ldap.NewServer()
+
+	routes := ldap.NewRouteMux()
+	routes.Bind(handleBind)
+	server.Handle(routes)
+
+	// listen on 10389
+	go server.ListenAndServe("127.0.0.1:10389")
+
+	// When CTRL+C, SIGINT and SIGTERM signal occurs
+	// Then stop server gracefully
+	ch := make(chan os.Signal)
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+	<-ch
+	close(ch)
+
+	server.Stop()
+}
+
+// handleBind return Success if login == mysql
+func handleBind(w ldap.ResponseWriter, m *ldap.Message) {
+	r := m.GetBindRequest()
+	res := ldap.NewBindResponse(ldap.LDAPResultSuccess)
+
+	//	if string(r.Name()) == "myLogin" {
+	w.Write(r.Name())
+	w.Write(res)
+	return
+	//}
+
+	// log.Printf("Bind failed User=%s, Pass=%s", string(r.Name()), string(r.AuthenticationSimple()))
+	// res.SetResultCode(ldap.LDAPResultInvalidCredentials)
+	// res.SetDiagnosticMessage("invalid credentials")
+	// w.Write(res)
+}
