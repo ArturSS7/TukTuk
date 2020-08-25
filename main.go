15ea701d18b9f0381cd3de526525794ba7dc64f9
ArturSS7 <52138851+ArturSS7@users.noreply.github.com>
2020-08-25 15:06:12 +0300

    fix main

--- main.go
+++ main.go
@@ -2,16 +2,12 @@ package main
 
 import (
 	"TukTuk/backend"
+	"TukTuk/config"
 	"TukTuk/database"
 	"TukTuk/dnslistener"
 	"TukTuk/emailalert"
 	"TukTuk/httplistener"
-	"fmt"
-
-	//"TukTuk/httpslistener"
-
-	//"TukTuk/httpslistener"
-	"TukTuk/config"
+	"TukTuk/httpslistener"
 	"TukTuk/smtplistener"
 	"TukTuk/telegrambot"
 )
@@ -19,12 +15,11 @@ import (
 func main() {
 	config.StartInit()
 	domain := config.Settings.DomainConfig.Name
-	fmt.Println(config.Settings.DomainConfig)
+
 	//start telegram bot
 	telegrambot.BotStart()
 	emailalert.EmailAlertStart(config.Settings.EmailAlert.Enabled, config.Settings.EmailAlert.To)
-	fmt.Println(config.Settings)
-	//fmt.Println(config.Settings.DomainConfig.Name[:len(config.Settings.DomainConfig.Name)-1])
+
 	//connect to database
 	db := database.Connect()
 
@@ -32,7 +27,7 @@ func main() {
 	go httplistener.StartHTTP(db)
 
 	//start https server
-	//go httpslistener.StartHTTPS(db)
+	go httpslistener.StartHTTPS(db)
 
 	//start dns server
 	go dnslistener.StartDNS(domain)
