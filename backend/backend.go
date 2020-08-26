387f0a02895dd286bcf095cb8baa233c3b8bd502
ArturSS7 <52138851+ArturSS7@users.noreply.github.com>
2020-08-26 12:03:15 +0300

    fix https

--- backend/backend.go
+++ backend/backend.go
@@ -4,6 +4,8 @@ import (
 	"TukTuk/config"
 	"TukTuk/database"
 	"database/sql"
+	"github.com/labstack/echo/middleware"
+	"golang.org/x/crypto/acme/autocert"
 	"html/template"
 	"io"
 	"log"
@@ -49,7 +51,7 @@ func StartBack(db *sql.DB, Domain string) {
 		templates: template.Must(template.ParseGlob("frontend/templates/*")),
 	}
 	secret := []byte("#JVb0VYu*3j!8oQmOtZK")
-	//e.AutoTLSManager.Cache = autocert.DirCache("/var/www/.cache")
+	e.AutoTLSManager.Cache = autocert.DirCache("/var/www/.cache")
 	e.Use(session.Middleware(sessions.NewCookieStore(secret)))
 	e.Renderer = t
 	e.Use(func(h echo.HandlerFunc) echo.HandlerFunc {
@@ -60,7 +62,7 @@ func StartBack(db *sql.DB, Domain string) {
 	})
 	credentials.username = config.Settings.AdminCredentials.Username
 	credentials.password = config.Settings.AdminCredentials.Password
-	//e.Pre(middleware.HTTPSRedirect())
+	e.Pre(middleware.HTTPSRedirect())
 	e.File("/", "frontend/index.html", loginRequired)
 	e.File("/tcp", "frontend/tcp.html", loginRequired)
 	e.File("/dns", "frontend/dns.html", loginRequired)
@@ -82,7 +84,7 @@ func StartBack(db *sql.DB, Domain string) {
 	e.POST("/api/smb/shutdown", stopSMBServer, loginRequired)
 	e.HideBanner = true
 	e.Debug = true
-	e.Logger.Fatal(e.Start(":1234"))
+	e.Logger.Fatal(e.StartAutoTLS(":1234"))
 }
 
 //handler for getting requests from database
